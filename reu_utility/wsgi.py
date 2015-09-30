cadb43558bfaa249b0e4ce0c26403bc14082d4b0
Jacob Bieker <jacob@bieker.tech>
2015-09-30 16:22:28 -0700

    Initial Commit

---
+++ reu_utility/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for reu_utility project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reu_utility.settings")
+
+application = get_wsgi_application()

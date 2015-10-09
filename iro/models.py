3746583c31cdd81ae5d7085392aae864abe8a3fc
Jacob Bieker <jacob@bieker.tech>
2015-10-09 11:37:35 -0700

    Add more to models

--- iro/models.py
+++ iro/models.py
@@ -6,8 +6,12 @@ class Applicant(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=40)
     applicant_name = models.CharField(max_length=200)
+    applicant_email = models.EmailField()
+    date_of_birth = models.DateField()
+
     mentors = models.ForeignKey(Mentor)
-    possible_pi = models.ForeignKey(Faculty)
+    possible_pis = models.ForeignKey(Faculty)
+
 
 class Mentor(models.Model):
     mentor_name = models.CharField(max_length=200)

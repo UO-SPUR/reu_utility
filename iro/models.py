670d35de02e3a742c4413443ae59b877b2912e58
Jacob Bieker <jacob@bieker.tech>
2015-10-09 11:31:08 -0700

    Start creation of models
    Mentor, Faculty, and Applicant

--- iro/models.py
+++ iro/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Applicant(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=40)
+    applicant_name = models.CharField(max_length=200)
+    mentors = models.ForeignKey(Mentor)
+    possible_pi = models.ForeignKey(Faculty)
+
+class Mentor(models.Model):
+    mentor_name = models.CharField(max_length=200)
+
+class Faculty(models.Model):
+    faculty_name = models.CharField(max_length=200)

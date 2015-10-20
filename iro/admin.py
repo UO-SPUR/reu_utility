3be50a0b9f824c8af8e5353b3c131e6992ff7ef3
Jacob Bieker <jacob@bieker.tech>
2015-10-20 10:20:12 -0700

    Create admin interface
    All show up successfully, behaving as expected.

--- iro/admin.py
+++ iro/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Applicant)
+admin.site.register(Faculty)
+admin.site.register(Mentor)
+admin.site.register(Intern)
+admin.site.register(Abstract)
+admin.site.register(ReferenceLetter)
+admin.site.register(ProgressReport)
+admin.site.register(PISurvey)
+admin.site.register(InternSurvey)
+admin.site.register(MentorSurvey)

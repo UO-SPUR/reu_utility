5f11d38bc039f8bec35f1f5f45d2a6772b975b2f
Jacob Bieker <jacob@bieker.tech>
2015-10-31 17:56:28 -0700

    Star creation to test cases

--- iro/tests.py
+++ iro/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from iro.models import *
 
 # Create your tests here.
+
+class ApplicantTestCase(TestCase):
+    def setUp(self):
+        Applicant.objects.create()
+
+class FacultyTestCase(TestCase):
+    def setUp(self):
+        Faculty.objects.create()
+
+class MentorTestCase(TestCase):
+    def setUp(self):
+        Mentor.objects.create()
+
+class InstituteTestCase(TestCase):
+    def setUp(self):
+        Institute.objects.create()
+
+class AbstractTestCase(TestCase):
+    def setUp(self):
+        Abstract.objects.create()
+
+class InternTestCase(TestCase):
+    def setUp(self):
+        Intern.objects.create()
+
+class ProgressReportTestCase(TestCase):
+    def setUp(self):
+        ProgressReport.objects.create()
+
+class IroSetupTestCase(TestCase):
+    def setUp(self):
+        IroSetup.objects.create()

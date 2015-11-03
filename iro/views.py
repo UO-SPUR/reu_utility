bf061e87ef30d3863e4433e839bfbe9e9935ac1b
Jacob Bieker <jacob@bieker.tech>
2015-11-03 11:19:19 -0800

    Add basic Faculty and Mentor form view

--- iro/views.py
+++ iro/views.py
@@ -20,3 +20,39 @@ def get_application(request):
         form = ApplicantForm()
 
     return render(request, 'application.html', {'application_form': form})
+
+def get_faculty(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = FacutlyForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = FacultyForm()
+
+    return render(request, 'faculty-sign-up.html', {'faculty_form': form})
+
+def get_mentor(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = MentorForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = MentorForm()
+
+    return render(request, 'mentor-sign-up.html', {'mentor_form': form})

464f9d1159387f76df4bc0a4fef90797e7cf466c
Jacob Bieker <jacob@bieker.tech>
2015-12-14 10:36:17 -0800

    Change some email settings

--- reu_utility/settings.py
+++ reu_utility/settings.py
@@ -118,8 +118,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 #Email settings
-DEFAULT_FROM_EMAIL = 'summerpgms@uoregon.edu'
-EMAIL_HOST = "uoregon.edu"
-EMAIL_PORT = "465"
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''

9ee008c64a777dfee88e95d4b47ce8e2e0bef5c2
Jacob Bieker <jacob@bieker.tech>
2015-12-14 11:33:40 -0800

    Add basic easyPDF view
    Source: https://django-easy-pdf.readthedocs.org/en/latest/usage.html

--- iro/urls.py
+++ iro/urls.py
@@ -2,9 +2,11 @@ __author__ = 'jacob'
 
 from django.conf.urls import url
 from . import views
+from iro.views import *
 
 urlpatterns = [
     url(r'^application', views.get_application, name='application'),
     url(r'^faculty-sign-up', views.get_faculty, name='faculty-sign-up'),
     url(r'^mentor-sign-up', views.get_mentor, name='mentor-sign-up'),
+    url(r"^hello.pdf$", HelloPDFView.as_view())
 ]

d08e42ac50408b47b62578cf0b7dabec2e0ea488
Jacob Bieker <jacob@bieker.tech>
2015-12-19 17:12:10 -0800

    Update Applicant model
    Default year_created now exists

--- iro/models.py
+++ iro/models.py
@@ -4,6 +4,7 @@ from django.core.validators import RegexValidator
 from iro.choices import *
 from django.core.exceptions import ValidationError
 import uuid
+from datetime import datetime
 
 # Allow only one model to be created (For Setup)
 def validate_only_one_instance(obj):
@@ -208,8 +209,8 @@ class Applicant(models.Model):
                                        null=True, max_length=10)
     comments = models.TextField("Admin Comments", help_text="Comments on Applicant")
     application_completeness = models.TextField("Application Completeness", help_text="Anything missing?")
-    correspondence = models.TextField("Correspondence", help_text="Correspondence")
-    year_created = models.DateField("Created Year", help_text="Year Created")
+    correspondence = models.TextField("Correspondence", help_text="Correspondence", null=True)
+    year_created = models.DateField("Created Year", help_text="Year Created", default=datetime.year)
     short_list = models.CharField("Short List?", choices=SHORT_LIST_CHOICES, default=UNSURE, max_length=10)
     transcript = models.FileField('Transcript', upload_to='transcripts', null=True)
     uuid = models.TextField("UUID", default=uuid.uuid4(), null=False)

996f57b0ed78f9fddaa5e931131469ac16a2ecc6
Jacob Bieker <jacob@bieker.tech>
2016-01-22 13:35:54 -0800

    Add testing fro creating Mentor

--- iro/tests/tests_registration.py
+++ iro/tests/tests_registration.py
@@ -17,7 +17,7 @@ class FacultyTestCase(TestCase):
         User.objects.create(username="iroUtility",
                             email="no-reply@example.com",
                             password="123456",
-                            groups=Group.objects.get(name="Faculty"),
+                            groups=Group.objects.get(name="Faculty")
                             )
         Faculty.objects.create(user=User.objects.get(username="iroUtility"),
                                faculty_name="Robert Benolken",
@@ -29,9 +29,43 @@ class FacultyTestCase(TestCase):
         self.assertEqual(faculty.user(), User.objects.get(username="iroUtility"))
         self.assertEqual(faculty.user.groups.filter(name="Faculty").exists(), True)
 
+
 class MentorTestCase(TestCase):
     def setUp(self):
-        Mentor.objects.create()
+        Institute.objects.create(name="Bieker Institute of Technology",
+                                 street="Einstein Blvd",
+                                 city="Geneva",
+                                 state="Switzerland",
+                                 zipcode="97412",
+                                 discipline="High-Energy Physics")
+        User.objects.create(username="iroUtility",
+                            email="no-reply@example.com",
+                            password="123456",
+                            groups=Group.objects.get(name="Faculty")
+                            )
+        Faculty.objects.create(user=User.objects.get(username="iroUtility"),
+                               faculty_name="Robert Benolken",
+                               institute=Institute.objects.get(city="Geneva"))
+        User.objects.create(username="iroMentor",
+                            email="no-reply@example.com",
+                            password="123456",
+                            groups=Group.objects.get(name="Mentor"),
+                            )
+        Mentor.objects.create(user=User.objects.get(username="iroMentor"),
+                              mentor_name="Mentor One",
+                              professor=Faculty.objects.get(faculty_name="Robert Benolken"))
+
+    def test_mentor_exists(self):
+        faculty = Faculty.objects.get(faculty_name="Robert Benolken")
+        mentor = Mentor.objects.get(mentor_name="Mentor One")
+        self.assertEqual(faculty.institute(), Institute.objects.get(city="Geneva"))
+        self.assertEqual(faculty.user(), User.objects.get(username="iroUtility"))
+        self.assertEqual(faculty.user.groups.filter(name="Faculty").exists(), True)
+
+        self.assertEqual(mentor.user(), User.objects.get(username="iroMentor"))
+        self.assertEqual(mentor.professor(), Faculty.objects.get(faculty_name="Robert Benolken"))
+        self.assertEqual(mentor.mentor_name(), "Mentor One")
+
 
 class InternTestCase(TestCase):
     def setUp(self):

18c3db21c7846196f5a6194b87a11af0c207ec9e
Jacob Bieker <jacob@bieker.tech>
2016-01-21 22:27:15 -0800

    Move tests to specific test ones

--- iro/tests/tests_interns.py
+++ iro/tests/tests_interns.py
@@ -1,1 +1,15 @@
 __author__ = 'jacob'
+
+from django.test import TestCase
+from django.utils import timezone
+from iro.models import *
+from iro.choices import *
+
+class AbstractTestCase(TestCase):
+    def setUp(self):
+        Abstract.objects.create()
+
+
+class ProgressReportTestCase(TestCase):
+    def setUp(self):
+        ProgressReport.objects.create()
--- iro/tests/tests_registration.py
+++ iro/tests/tests_registration.py
@@ -1,1 +1,19 @@
 __author__ = 'jacob'
+from django.test import TestCase
+from django.utils import timezone
+from iro.models import *
+from iro.choices import *
+
+
+class FacultyTestCase(TestCase):
+    def setUp(self):
+        Faculty.objects.create()
+
+
+class MentorTestCase(TestCase):
+    def setUp(self):
+        Mentor.objects.create()
+
+class InternTestCase(TestCase):
+    def setUp(self):
+        Intern.objects.create()

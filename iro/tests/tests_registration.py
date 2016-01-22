2bfc496f420eab8405c850b076af45c9b283e6a5
Jacob Bieker <jacob@bieker.tech>
2016-01-22 13:25:48 -0800

    Add creating Faculty for test

--- iro/tests/tests_registration.py
+++ iro/tests/tests_registration.py
@@ -1,6 +1,6 @@
 __author__ = 'jacob'
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.utils import timezone
 from iro.models import *
 from iro.choices import *
@@ -14,9 +14,14 @@ class FacultyTestCase(TestCase):
                                  state="Switzerland",
                                  zipcode="97412",
                                  discipline="High-Energy Physics")
-        User.objects.create()
-        Faculty.objects.create(faculty_name="Robert Benolken",
-                               institute=)
+        User.objects.create(username="iroUtility",
+                            email="no-reply@example.com",
+                            password="123456",
+                            groups=Group.objects.get(name="Faculty"),
+                            )
+        Faculty.objects.create(user=User.objects.get(username="iroUtility"),
+                               faculty_name="Robert Benolken",
+                               institute=Institute.objects.get(city="Geneva"))
 
 
 class MentorTestCase(TestCase):

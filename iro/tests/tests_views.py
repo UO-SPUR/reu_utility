b97c9d21f2559e769c3a7af25d10c2065190a68d
Jacob Bieker <jacob@bieker.tech>
2016-01-23 20:40:41 -0800

    Add for testing views

---
+++ iro/tests/tests_views.py
@@ -0,0 +1,6 @@
+__author__ = 'jacob'
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+from iro.models import *
+from iro.choices import *

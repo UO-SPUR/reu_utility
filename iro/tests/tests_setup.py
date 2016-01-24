ffd98974b27add4a2ac6097ce1fd272503337d48
Jacob Bieker <jacob@bieker.tech>
2016-01-23 21:22:04 -0800

    Testing IROSetup creation

--- iro/tests/tests_setup.py
+++ iro/tests/tests_setup.py
@@ -12,5 +12,23 @@ class InstituteTestCase(TestCase):
 
 
 class IroSetupTestCase(TestCase):
-    def setUp(self):
-        IroSetup.objects.create()
+    def test_setup_creation(self):
+        setup = IroSetup.objects.create(program_name="Summer Program For Undergraduate Research",
+                                        acronym="SPUR",
+                                        base_url="spur.uoregon.edu",
+                                        university_name="University of Oregon",
+                                        program_email="summerpgms@uoregon.edu",
+                                        program_director="Jacob Bieker",
+                                        program_phone_number=5552221111)
+
+        self.assertEqual(setup.acronym, "SPUR")
+        self.assertEqual(setup.program_phone_number, 5552221111)
+
+        self.assertEqual(IroSetup.objects.create(program_name="Summer Program For Undergraduate Research",
+                                                 acronym="SPUR",
+                                                 base_url="spur.uoregon.edu",
+                                                 university_name="University of Oregon",
+                                                 program_email="summerpgms@uoregon.edu",
+                                                 program_director="Jacob Bieker",
+                                                 program_phone_number=5552221111), ValidationError)
+
